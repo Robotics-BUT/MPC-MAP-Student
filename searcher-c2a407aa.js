03dca9644be02ec2bd1b450b7b97df4bb5e2bbc4
petrgabrlik <petrgabrlik@users.noreply.github.com>
2026-03-24 13:04:59 +0000

    deploy: 04ce9121859abb63ee1a1cbc0194bd0d8e465ed7

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c3fdd5ca.js',
+                path_to_root + 'searchindex-ceab1c8d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
