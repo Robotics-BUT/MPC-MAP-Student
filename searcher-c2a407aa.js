dbfb3ef13b5963eac2440ddfc8abb1998b645a87
tlazna <tlazna@users.noreply.github.com>
2026-04-20 08:18:24 +0000

    deploy: 9d7d2ff1d1a26136c8f465c07bbaa2cdddc2ba61

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-093732ba.js',
+                path_to_root + 'searchindex-546bcd63.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
